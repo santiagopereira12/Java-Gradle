8d4fb1d44742f0c4280da70ba6177eb1af4a1785
santiagopereira12 <spereira98@misena.edu.co>
2025-11-27 09:16:02 -0500

    Configuración inicial de proyecto Spring Boot con Java 21

---
+++ src/main/java/com/platzi/play/PlatziPlayApplication.java
@@ -0,0 +1,14 @@
+package com.platzi.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziPlayApplication.class, args);
+		System.out.println("Hola Mundo Spring Boot");
+	}
+
+}
