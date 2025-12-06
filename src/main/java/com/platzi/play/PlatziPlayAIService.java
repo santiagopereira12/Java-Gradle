882e5dd8d1319371b41e0a2b4ccd76473b46d768
santiagopereira12 <spereira98@misena.edu.co>
2025-12-06 18:57:10 -0500

    Configuracion de application.properties y perfiles en spring boot

--- src/main/java/com/platzi/play/PlatziPlayAIService.java
+++ src/main/java/com/platzi/play/PlatziPlayAIService.java
@@ -1,13 +1,14 @@
 package com.platzi.play;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface PlatziPlayAIService {
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de gestion de Peliculas PlatziPlay.
+            Genera un saludo de bienvenida a la plataforma de gestion de Peliculas {{plataform}}.
             Usa menos de 120 caracteres y hazlo con el estilo de platzi
             """)
-    String generateGreeting();
+    String generateGreeting(@V("plataform") String plataform);
 }
