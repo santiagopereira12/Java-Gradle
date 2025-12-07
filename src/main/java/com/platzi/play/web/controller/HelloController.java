d6a936ec4a7d69a159299b657d757fb90f231c34
santiagopereira12 <spereira98@misena.edu.co>
2025-12-07 15:41:00 -0500

    Arquitectura por capas orientada al dominio en java

--- src/main/java/com/platzi/play/web/controller/HelloController.java
+++ src/main/java/com/platzi/play/web/controller/HelloController.java
@@ -1,5 +1,6 @@
-package com.platzi.play;
+package com.platzi.play.web.controller;
 
+import com.platzi.play.domain.service.PlatziPlayAIService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class HelloController {
         this.iaService = iaService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello(){
         return this.iaService.generateGreeting(plataform);
     }
