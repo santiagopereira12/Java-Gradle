33700a35cc41cf4be904cda23fa36e0f9941a92d
santiagopereira12 <spereira98@misena.edu.co>
2025-12-29 15:49:22 -0500

    Crear endpoint POST para guardar peliculas en Spring Boot

--- src/main/java/com/platzi/play/domain/repository/MovieRepository.java
+++ src/main/java/com/platzi/play/domain/repository/MovieRepository.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface MovieRepository {
     List<MovieDto> getAll();
     MovieDto getById(long id);
+    MovieDto save(MovieDto movieDto);
 }
--- src/main/java/com/platzi/play/web/controller/MovieController.java
+++ src/main/java/com/platzi/play/web/controller/MovieController.java
@@ -2,11 +2,9 @@ package com.platzi.play.web.controller;
 
 import com.platzi.play.domain.dto.MovieDto;
 import com.platzi.play.domain.service.MovieService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +30,11 @@ public class MovieController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(movieDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieDto> add(@RequestBody MovieDto movieDto){
+        MovieDto movieDtoResponse = this.movieService.add(movieDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(movieDtoResponse);
     }
 }
